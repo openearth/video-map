75b8671537bd5ada1c611b0810b62bf2008769f1
gena <gennadiy.donchyts@gmail.com>
2019-11-12 14:21:37 +0100

    fix script paramaters

--- python-functions/convert-gee-map-png2webm.py
+++ python-functions/convert-gee-map-png2webm.py
@@ -13,7 +13,7 @@ def run(cmd):
     subprocess.run(cmd)
 # folders = ['classificatie', 'ndvi', 'satellite-false', 'satellite-natural']
 bucket_name = "D:/data/eo-bathymetry"
-folder = "sdb-rgb-test4"
+folder = "sdb-rgb-v3.3"
 temp_dir = "temp"
 video_folder = folder+"-video"
 
@@ -59,7 +59,9 @@ for zoom in zoom_levels:
             new_path = r'{0}/{1}/{2}/{3}'.format(bucket_name, video_folder, zoom, tile_x)
             if not os.path.exists(new_path):
                 os.makedirs(new_path)
-            cmd = 'ffmpeg -framerate 10 -i "{0}/{1}/%03d.png" -c:v libvpx -an -qmin 0 -qmax 30 -crf 5 -auto-alt-ref 0 {2}/{1}.webm -y'.format(tile_x_dir, tile_y, new_path)
+            # make sure we get keyframe every frame
+            # see https://stackoverflow.com/questions/24152810/encoding-ffmpeg-to-mpeg-dash-or-webm-with-keyframe-clusters-for-mediasource/37776212
+            cmd = 'ffmpeg -framerate 10 -i "{0}/{1}/%03d.png" -c:v libvpx -keyint_min 1 -cluster_size_limit 10M -cluster_time_limit 2100 -g 1 -an -qmin 0 -qmax 30 -crf 5 -auto-alt-ref 0 {2}/{1}.webm -y'.format(tile_x_dir, tile_y, new_path)
             run(cmd)
 
 # upload to storage example:
